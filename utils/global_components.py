e4d13853a7eacc671311f53f223b9de1de5635ef
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-03-26 18:34:35 +0000

    doing full perpca

--- utils/global_components.py
+++ utils/global_components.py
@@ -72,44 +72,25 @@ def global_variance_explained(U, covs):
     den = sum(torch.trace(S) for S in covs)
     return (num / den).item()
 
-def make_synthetic(num_clients=5, d=50, n=500, true_r1=6, true_r2=2, noise_std=0.4):
+def make_synthetic(num_clients=5, d=50, n=500, true_r1=6, true_r2=2, noise_std=1.0):
     U_true = torch.linalg.qr(torch.randn(d, true_r1, device=device))[0]
     Ys = []
     for _ in range(num_clients):
         V_true = torch.linalg.qr(torch.randn(d, true_r2, device=device))[0]
         shared = U_true @ torch.randn(true_r1, n, device=device)
         local  = V_true @ torch.randn(true_r2, n, device=device)
-        Ys.append(shared + local + noise_std * torch.randn(d, n, device=device))
+        Ys.append((shared + local + noise_std * torch.randn(d, n, device=device)).detach().cpu().numpy())
     return Ys
 
 
 def get_num_global_components(clients):
     covs = [(Y @ Y.T) / Y.shape[1] for Y in clients]
 
-    candidate_r1 = list(range(1, 381))
-    gv = pooled_scree(covs, max_components=380)
-
-    print(gv)
-
-    # 2nd‑difference elbow
-    second_diff = np.abs(np.diff(gv, n=7))
-    elbow_2nd = candidate_r1[np.argmax(second_diff) + 1]
-
-    print("Shared‑variance fractions:", np.round(gv, 3))
-    print("Elbow (2nd diff) =", elbow_2nd)
-
-
-if __name__ == "__main__":
-
-
-    clients = make_synthetic(num_clients=5, d=384, n=500, true_r1=50, true_r2=300, noise_std=0.1)
-    covs    = [(Y @ Y.T) / Y.shape[1] for Y in clients]
-
-    candidate_r1 = list(range(20, 101))
+    candidate_r1 = list(range(1, 20))
     gv = []
 
     for r1 in tqdm(candidate_r1):
-        model = PerPCA(r1=r1, r2=383, eta=0.01, tol=1e-3)
+        model = PerPCA(r1=r1, r2=383, eta=0.01, tol=5e-2)
         U, _ = model.fit(clients)
         gv.append(global_variance_explained(U.to(device), covs))
 
@@ -118,4 +99,31 @@ if __name__ == "__main__":
     optimal_r1 = candidate_r1[np.argmax(second_diff) + 1]
 
     print("Elbow (2nd diff) =", optimal_r1)
+
+
+
+
+if __name__ == "__main__":
+
+
+    clients = make_synthetic(num_clients=5, d=384, n=500, true_r1=50, true_r2=300, noise_std=0.1)
+
+    get_num_global_components(clients)
+
+
+    # covs    = [(Y @ Y.T) / Y.shape[1] for Y in clients]
+
+    # candidate_r1 = list(range(20, 101))
+    # gv = []
+
+    # for r1 in tqdm(candidate_r1):
+    #     model = PerPCA(r1=r1, r2=383, eta=0.01, tol=1e-3)
+    #     U, _ = model.fit(clients)
+    #     gv.append(global_variance_explained(U.to(device), covs))
+
+    # gv = np.array(gv)
+    # second_diff = np.abs(np.diff(gv, n=2))
+    # optimal_r1 = candidate_r1[np.argmax(second_diff) + 1]
+
+    # print("Elbow (2nd diff) =", optimal_r1)
 
