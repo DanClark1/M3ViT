396e80d04f5c8230152c3a4ac07f26a3c171a3ed
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-03-27 15:05:16 +0000

    utils

--- utils/global_components.py
+++ utils/global_components.py
@@ -108,7 +108,8 @@ def get_num_global_components(clients):
 
     model = PerPCA(r1=max_r1, r2=384, eta=0.01, tol=1e-3)
     U, V = model.fit(clients)
-
+    V = torch.Tensor(V).to(device)
+    print(V.shape)
     for i in tqdm(range(U.shape[1])):
         V_subset = U[:, :, :i]
 
