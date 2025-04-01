25451e27db1854a01417253d172ef8c85ec0e1f9
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-02 00:45:20 +0100

    given up on writing commit messages

--- train/train_utils.py
+++ train/train_utils.py
@@ -287,7 +287,7 @@ def train_vanilla_distributed(args, p, train_loader, model, criterion, optimizer
 
 
 
-def calculate_moe_diversity_loss(model, coefficient=0.1):
+def calculate_moe_diversity_loss(model, coefficient=1):
     '''
     Takes the an image in a batch and computes the diversity loss (assuming model is moe)
 
