fa4f10d2106f129ab9412ce1e8acceb496be6cdd
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-02 15:12:58 +0100

    given up on writing commit messages

--- train/train_utils.py
+++ train/train_utils.py
@@ -249,7 +249,8 @@ def train_vanilla_distributed(args, p, train_loader, model, criterion, optimizer
            
             if p['backbone'] == 'VisionTransformer_moe' and (not args.moe_data_distributed):
                 loss_dict['total'] += collect_noisy_gating_loss(model, args.moe_noisy_gate_loss_weight)
-                loss_dict['total'] += calculate_moe_diversity_loss(model)
+                diversity_loss = calculate_moe_diversity_loss(model)
+                loss_dict['total'] += diversity_loss
                 #print(loss_dict['total'])
                 #print(calculate_moe_cosine_similarity_loss(model).shape)
                 # Force both to be scalars before summing, then restore shape if necessary
@@ -279,7 +280,7 @@ def train_vanilla_distributed(args, p, train_loader, model, criterion, optimizer
             
             
         if i % 25 == 0:
-            print('similarity_loss',similarity_loss)
+            print('similarity_loss',diversity_loss)
             progress.display(i)
             # for name, param in model.named_parameters():
             #     if 'gamma' in name:
