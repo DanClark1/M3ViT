c884d30115070cae885d62be4ef7db27fb2405a3
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-07 17:57:54 +0100

    given up on writing commit messages

--- utils/factorise_model.py
+++ utils/factorise_model.py
@@ -6,25 +6,25 @@ from utils.common_config import build_val_dataloader
 @torch.no_grad()
 def factorise_model(p, val_dataset, model, n=50, distributed=False):
     """ Evaluate model in an online fashion without storing the predictions to disk """
-    tasks = p.TASKS.NAMES
-    subset_ratio = n / len(val_dataset)
-    val_indices = random.sample(range(len(val_dataset)), int(len(val_dataset) * subset_ratio))
-    val_dataset = Subset(val_dataset, val_indices)
+    # tasks = p.TASKS.NAMES
+    # subset_ratio = n / len(val_dataset)
+    # val_indices = random.sample(range(len(val_dataset)), int(len(val_dataset) * subset_ratio))
+    # val_dataset = Subset(val_dataset, val_indices)
 
-    val_dataloader = build_val_dataloader(
-        val_dataset, p['valBatch'], p['nworkers'], dist=distributed)
+    # val_dataloader = build_val_dataloader(
+    #     val_dataset, p['valBatch'], p['nworkers'], dist=distributed)
 
-    model.eval()
+    # model.eval()
 
-    print('recording expert outputs...')
+    # print('recording expert outputs...')
 
-    for i, batch in enumerate(val_dataloader):
-        # Forward pass
-        images = batch['image'].cuda(non_blocking=True)
-        print('images shape:', images.shape)
-        targets = {task: batch[task].cuda(non_blocking=True) for task in tasks}
-        output = model(images, isval=True)
+    # for i, batch in enumerate(val_dataloader):
+    #     # Forward pass
+    #     images = batch['image'].cuda(non_blocking=True)
+    #     print('images shape:', images.shape)
+    #     targets = {task: batch[task].cuda(non_blocking=True) for task in tasks}
+    #     output = model(images, isval=True)
 
-    model.module.dump_output_matricies()
+    # model.module.dump_output_matricies()
     print('factorising model...')
     model.module.factorise_model()
