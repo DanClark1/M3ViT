cb89dbcefd47507b58bc4968f7b1503b225732e3
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-08 19:15:48 +0100

    given up on writing commit messages

--- train/train_utils.py
+++ train/train_utils.py
@@ -252,7 +252,7 @@ def train_vanilla_distributed(args, p, train_loader, model, criterion, optimizer
             matricies = []
            
             if p['backbone'] == 'VisionTransformer_moe' and (not args.moe_data_distributed):
-                diversity_loss_coeff = 5 # might be too high tbf
+                diversity_loss_coeff = 20 # might be too high tbf
                 main_loss = loss_dict['total']
                 gating_loss = collect_noisy_gating_loss(model, args.moe_noisy_gate_loss_weight)
                 loss_dict['total'] += gating_loss
