952cb0910986321c86377c30d172f0f68384da66
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-08 11:49:09 +0100

    given up on writing commit messages

--- models/custom_moe_layer.py
+++ models/custom_moe_layer.py
@@ -510,6 +510,7 @@ class FMoETransformerMLP(FMoE):
 
 
         if record_outputs:
+                print('a')
                 # moe_outp shape here is (batch_positions, top_k, dim) for non-factorised
                 # or (batch_positions, top_k + 1, dim) if factorised
                 # We'll assume non-factorised for simplicity
@@ -573,6 +574,7 @@ class FMoETransformerMLP(FMoE):
                 
                 self.loss += lambda_max
                 self.loss_normalise_weight += 1
+                print('b')
 
         def bmm_func(tensor):
             dim = tensor.shape[-1]
