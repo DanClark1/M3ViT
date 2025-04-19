6fa007f2240e657309fd61231432169c2d0acb72
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-19 15:11:24 +0100

    optimised lambda

--- models/custom_moe_layer.py
+++ models/custom_moe_layer.py
@@ -463,9 +463,7 @@ class FMoETransformerMLP(FMoE):
         eigvals = torch.linalg.eigvalsh(avg_proj)
         lambda_max = eigvals[-1]
         
-        self.lambda_max_loss += lambda_max
-        self.lambda_max_normalise_weight += 1
-
+        return lambda_max
 
     def calculate_lambda_max_loss(self, moe_outp, gate_top_k_idx):
 
