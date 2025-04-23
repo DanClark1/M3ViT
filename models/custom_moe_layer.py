3fd65cf8827317ad9944e39f86b1c68971a3aa0f
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-23 10:01:56 +0100

    subspace enforcement

--- models/custom_moe_layer.py
+++ models/custom_moe_layer.py
@@ -362,7 +362,7 @@ class FMoETransformerMLP(FMoE):
         #moe_outp = gram_schmidt_orthonormalize(moe_outp)
 
         projected_moe_outp = project_to_unique_subspaces(moe_outp[:, :-1 :], self.rawB)
-        moe_outp = torch.cat((projected_moe_outp, moe_outp[:, -1, :]), dim=1)
+        moe_outp = torch.cat((projected_moe_outp, moe_outp[:, -1:, :]), dim=1)
 
 
         def bmm_func(tensor):
